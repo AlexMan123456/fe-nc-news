fca1ee2dc4150b0c76336693dc33bb85e24fa62f
AlexMan123456 <alex.man03888@gmail.com>
2024-11-04 14:16:05 +0000

    Made image smaller with CSS

--- components/articles/ArticleCard.jsx
+++ components/articles/ArticleCard.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 function ArticleCard(props){
     return (<>
         <img className="article-image" key={`article-${props.article_id}-image`} src={props.image} alt={`cover image for article '${props.title}' by ${props.author}`}/>
+        <br></br>
         <Link to={`/articles/${props.article_id}`} key={`article-${props.article_id}-title`}>{props.title}</Link>
         <p key={`article-${props.article_id}-author`}>Created by {props.author} on {props.created_at}</p>
         <p key={`article-${props.article_id}-votes`}>Votes: {props.votes}</p>
