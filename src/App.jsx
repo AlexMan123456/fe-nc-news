b7e99cec238b6cc71e1e9b491d35121cebc20a45
AlexMan123456 <alex.man03888@gmail.com>
2024-11-04 14:13:37 +0000

    Added back routes to App.jsx, even though I swear I had added it previously

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+import ArticlesList from "../components/articles/ArticlesList"
+import Homepage from "../components/Homepage"
+import Navigation from "../components/Navigation"
+
 function App() {
-  return <h1>Test</h1>
+  return (<>
+    <h1>NC News</h1>
+    <Navigation/>
+    <Routes>
+      <Route path="/" element={<Homepage/>}/>
+      <Route path="/articles" element={<ArticlesList/>}/>
+    </Routes>
+  </>)
 }
 
 export default App
