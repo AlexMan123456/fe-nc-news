829b3d6c2421a8384561f8ce8bd2190f003aa95d
AlexMan123456 <alex.man03888@gmail.com>
2024-11-06 14:38:34 +0000

    Hopefully this will fix the homepage image in deployed build

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,7 +1,9 @@
+import welcomeImage from "../images/welcome-image.jpeg"
+
 function Homepage(){
     return (<>
         <h2>Welcome to NC News!</h2>
-        <img src="../images/welcome-image.jpeg" alt="A wooden surface with newspaper, teacup, notebook, and a portion of a pair of glasses on the right"/>
+        <img src={welcomeImage} alt="A wooden surface with newspaper, teacup, notebook, and a portion of a pair of glasses on the right"/>
     </>)
 }
 
