6090639544ae12e010c7fed78ad5ae3b317d828a
AlexMan123456 <alex.man03888@gmail.com>
2024-11-06 16:16:59 +0000

    Styled links to change colour when hovered over

--- components/articles/ArticleCard.jsx
+++ components/articles/ArticleCard.jsx
@@ -4,7 +4,7 @@ import formatDateAndTime from "../../utils/format-date-and-time"
 function ArticleCard(props){
     const {article_id, title, author, article_img_url: image, created_at, votes, comment_count, topic} = props.article
     const {date, time} = formatDateAndTime(created_at)
-    return (<div key={`article-${article_id}-card`} className="article-card">
+    return (<fieldset key={`article-${article_id}-card`} className="article-card">
         <img 
             className="article-image" 
             key={`article-${article_id}-image`} 
@@ -13,7 +13,8 @@ function ArticleCard(props){
         <br></br>
         <Link 
             to={`/articles/${article_id}`}
-            key={`article-${article_id}-title`}>
+            key={`article-${article_id}-title`}
+            className="article-link">
                 {title}
         </Link>
         <br></br>
@@ -30,9 +31,11 @@ function ArticleCard(props){
             key={`article-${article_id}-author`}>
                 Created by {author} on {date} at {time}
         </label>
-        <label key={`article-${article_id}-votes`}>Votes: {votes}</label>
-        <label key={`article-${article_id}-comment-count`}>Comments: {comment_count}</label>
-    </div>)
+        <div id="article-stat-counts">
+            <label className="article-stat-count" key={`article-${article_id}-votes`}>Votes: {votes}</label>
+            <label className="article-stat-count" key={`article-${article_id}-comment-count`}>Comments: {comment_count}</label>
+        </div>
+    </fieldset>)
 }
 
 export default ArticleCard
