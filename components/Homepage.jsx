2b746812ad4337f66366ec17ee3da39b6ff46cc7
AlexMan123456 <alex.man03888@gmail.com>
2024-11-07 12:15:51 +0000

    Made homepage image smaller and more responsive, and made centering of author label in article responsive

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,10 +1,10 @@
 import welcomeImage from "../images/welcome-image.jpeg"
 
 function Homepage(){
-    return (<>
+    return (<div id="homepage">
         <h2>Welcome to NC News!</h2>
         <img src={welcomeImage} alt="A wooden surface with newspaper, teacup, notebook, and a portion of a pair of glasses on the right"/>
-    </>)
+    </div>)
 }
 
 export default Homepage
