0978b7857b07ca44c718aee300a11f3aecfab929
AlexMan123456 <alex.man03888@gmail.com>
2024-12-14 15:03:22 +0000

    Fixed issue with image not appearing sometimes

--- components/articles/create-article/EditPage.jsx
+++ components/articles/create-article/EditPage.jsx
@@ -6,7 +6,8 @@ function EditPage(props){
     const {title, setTitle, body, setBody, topic, setTopic, image, setImage, setIsPreviewPage} = props
     const [allTopics, setAllTopics] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(false)
+    const [topicError, setTopicError] = useState("")
+    const [imageError, setImageError] = useState("")
     const articleTextboxRef = useRef(0)
     
     useEffect(() => {
@@ -15,7 +16,7 @@ function EditPage(props){
             setIsLoading(false)
             setAllTopics(topics)
         }).catch((err) => {
-            setError("An error has occured. Please try again later.")
+            setTopicError("An error has occured. Please try again later.")
         })
     }, [])
 
@@ -38,15 +39,29 @@ function EditPage(props){
     }
 
     function handleImage(event){
-        setImage(URL.createObjectURL(event.target.files[0]))
+        event.preventDefault()
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader()
+            reader.readAsDataURL(event.target.files[0])
+            reader.onload = () => {
+                resolve(reader.result)
+            }
+            reader.onerror = (err) => {
+                reject(err)
+            }
+        }).then((base64URL) => {
+            setImage(base64URL)
+        }).catch((err) => {
+            setImageError(err)
+        })
     }
 
     if(isLoading){
         return <p>Now loading...</p>
     }
 
-    if(error){
-        return <p>{error}</p>
+    if(topicError){
+        return <p>{topicError}</p>
     }
 
     return (<form id="article-creator" onSubmit={handleSubmit}>
@@ -60,7 +75,7 @@ function EditPage(props){
                 ></input>
         </div>
         <input type="file" onChange={handleImage}/>
-        {image ? <img src={image} alt="Your chosen image"/> : null}
+        {image ? <img src={image} alt="Your chosen image"/> : <p>{imageError}</p>}
         <div id="article-body-setter">
             <label htmlFor="article-body-textbox">Body:</label>
                 <textarea 
